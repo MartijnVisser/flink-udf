9505a6bb23a05af7f2bdf4764123252774a8398e
Martijn Visser <2989614+MartijnVisser@users.noreply.github.com>
2025-09-24 21:18:46 +0200

    Fix SQL statement

--- sql/custom_tax_demo.sql
+++ sql/custom_tax_demo.sql
@@ -1,9 +1,3 @@
--- Static SQL statement demonstrating the CustomTax UDF
-SELECT 
-    f0 AS product,
-    f1 AS location,
-    f2 * CustomTax(f1) AS tax
-FROM VALUES 
-    ('Apple', 'USA', 2),
-    ('Apple', 'EU', 3)
-AS t(f0, f1, f2);
+SELECT product, location, CustomTax(location)
+   FROM (VALUES ('Apple', 'USA', 2), ('Apple', 'EU', 3))
+   AS t (product, location, tax);
